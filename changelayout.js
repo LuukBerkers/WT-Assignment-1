1662320667634e023a667cbde37e7872c555e512
MarkLooye <marklooye1@gmail.com>
2020-02-28 15:59:33 +0100

    Eerste versie van dynamisch aanpassen van webpagina

--- changelayout.js
+++ changelayout.js
@@ -1,1 +1,26 @@
-console.log('jo');
+var items = [];
+var elementDropdown;
+var changeableElements = ['BODY', 'HEADER', 'FOOTER', 'ASIDE', 'ARTICLE', 'SECTION'];
+
+window.onload = function (){
+    elementDropdown = document.getElementById("elementDropdown");
+    var allElements = document.getElementsByTagName("*");
+
+    for (var element of allElements){
+        if (changeableElements.includes(element.tagName)){
+            items.push(element);
+        }
+    }
+
+    items.forEach(item => {
+        var element = document.createElement("option");
+        element.textContent = item.tagName;
+        elementDropdown.appendChild(element);
+    });
+}
+
+function onClick(){
+    var index = elementDropdown.options[elementDropdown.selectedIndex].index;
+    var element = items[index];
+    element.style["color"] = "red";
+}
