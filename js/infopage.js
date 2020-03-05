87a57a248f3340a973893c209f63085d859a6c9c
Luuk Berkers <berkers.luuk@gmail.com>
2020-03-05 11:44:40 +0100

    moved main functionality to functions

--- js/infopage.js
+++ js/infopage.js
@@ -1,13 +1,31 @@
+/**
+ * Create a new HTML element and append it to another element
+ * @param {string} tag The tag name of the HTML DOM element to be created
+ * @param {Node} parent The parent element to which the new element should be appended
+ */
+function makeElement(tag, parent) {
+  var element = document.createElement(tag);
+  parent.appendChild(element);
+
+  return element;
+}
+
+/**
+ * Create a text node and append it to an element as its child
+ * @param {string} text String that contains the text to be added to the node
+ * @param {Node} parent The parent element to which the text node should be appended
+ */
+function addText(text, parent) {
+  var textNode = document.createTextNode(text);
+  parent.appendChild(textNode);
+}
+
 function fillPage() {
   var body = document.querySelector("body");
 
-  var header = document.createElement("header");
-  body.appendChild(header);
-  var h1 = document.createElement("h1");
-  header.appendChild(h1);
-  var h1Text = document.createTextNode("Databases");
-  h1.appendChild(h1Text);
-
+  var header = makeElement("header", body);
+  var h1 = makeElement("h1", header);
+  addText("Databases", h1);
 }
 
 window.addEventListener("load", fillPage, false);
